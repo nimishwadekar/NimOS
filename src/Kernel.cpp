2542ed0c07383930a1cdec5d21d491e99969214f
Nimish Wadekar <wadekar.nimish@gmail.com>
2021-07-12 20:03:24 +0530

    Linked list test using Malloc()

--- src/Kernel.cpp
+++ src/Kernel.cpp
@@ -4,6 +4,12 @@
 #include "Logging.hpp"
 #include "Memory/Heap.hpp"
 
+struct node
+{
+    int data;
+    node *next;
+};
+
 // Kernel's main function.
 void KernelStart(void)
 {
@@ -14,18 +20,24 @@ void KernelStart(void)
     MainRenderer.ClearScreen();
     MainRenderer.Printf("Kernel initialized.\n");
 
-    void *malloced = Malloc(0x8000);
-    MainRenderer.Printf("Malloced 0x%x\n", malloced);
-    void *alloc = Malloc(0x8000);
-    MainRenderer.Printf("Malloced 0x%x\n", alloc);
-    malloced = Malloc(0x200);
-    MainRenderer.Printf("Malloced 0x%x\n", malloced);
-    malloced = Malloc(0x200);
-    MainRenderer.Printf("Malloced 0x%x\n", malloced);
-    Free(alloc);
-    MainRenderer.Printf("Malloced 0x%x\n", Malloc(0x8001));
-    MainRenderer.Printf("Malloced 0x%x\n", Malloc(0x6000));
-    MainRenderer.Printf("Malloced 0x%x\n", Malloc(0x400));
+    int a[] = {3, 19, 423, 936, 114, 11463, 9678};
+    node *head = (node*) Malloc(sizeof(node));
+    head->data = a[0];
+    node *cur = head;
+    for(int i = 1; i < 7; i++)
+    {
+        cur->next = (node*) Malloc(sizeof(node));
+        cur = cur->next;
+        cur->data = a[i];
+        cur->next = NULL;
+    }
+
+    cur = head;
+    while(cur)
+    {
+        MainRenderer.Printf("%u ", cur->data);
+        cur = cur->next;
+    }
 
     MainRenderer.Printf("\nKernel ended.\n");
 
