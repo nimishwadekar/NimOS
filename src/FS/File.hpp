40d92629e634cdb4b306b21c199622392705e6fc
Nimish Wadekar <wadekar.nimish@gmail.com>
2021-08-05 18:50:35 +0530

    Modify FILE struct

--- src/FS/File.hpp
+++ src/FS/File.hpp
@@ -9,15 +9,15 @@
 #define FS_APPEND (1 << 4)
 
 // Including null terminator.
-#define FILENAME_MAX_NULL 66
-#define DIRECTORY_MAX_NEST (FILENAME_MAX_NULL >> 1)
+#define FILENAME_MAX_NULL 255
+//#define DIRECTORY_MAX_NEST (FILENAME_MAX_NULL >> 1)
 
 struct FILE
 {
-    char Name[FILENAME_MAX_NULL];
+    char Name[FILENAME_MAX_NULL + 1];
     uint32_t Flags;
     uint8_t Device; // Volume, 'A' to 'Z'.
     uint64_t Length;
-    uint64_t CurrentCluster;
-    uint64_t Position; // Offset in current cluster.
+    uint64_t CurrentBlock;
+    uint64_t Position; // Offset in current block.
 };
