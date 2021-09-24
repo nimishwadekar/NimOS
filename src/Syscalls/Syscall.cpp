bb06fc534314fad2c46bff11d7df0c4cbbebae19
Nimish Wadekar <wadekar.nimish@gmail.com>
2021-09-25 02:08:53 +0530

    Add TODO

--- src/Syscalls/Syscall.cpp
+++ src/Syscalls/Syscall.cpp
@@ -2,6 +2,8 @@
 #include <Display/Renderer.hpp>
 #include <Syscalls/Syscall.hpp>
 
+// Add parameters and call syscall 1 from program.asm
+
 extern "C" void SyscallHandler()
 {
     printf("Here syscall %u\n", SYS_PRINT);
