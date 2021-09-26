c0e750c2d7f2088244468c043b7ed4e81813e1a2
Nimish Wadekar <wadekar.nimish@gmail.com>
2021-09-27 01:58:14 +0530

    Remove location macros

--- include/Usermode/Usermode.hpp
+++ include/Usermode/Usermode.hpp
@@ -2,8 +2,6 @@
 
 #include <stdint.h>
 
-#define USER_CODE_BASE 0x500000000
-#define USER_DATA_BASE 0x600000000
-#define USER_STACK_TOP 0x700000000
+#define USER_STACK_TOP 0x1000000
 
 extern "C" void JumpToUserMode(void *syscallEntryAddress, void *userRSP, void *programAddress);
