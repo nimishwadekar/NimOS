d70969393df981efad1a4a4a42cdd6f1aa76d2fa
Nimish Wadekar <wadekar.nimish@gmail.com>
2021-09-30 00:05:23 +0530

    Implement rand

--- usr/libc/stdlib/rand.c
+++ usr/libc/stdlib/rand.c
@@ -1,12 +1,15 @@
 #include <stdlib.h>
 
+static unsigned long int next = 1;
+
 int rand(void)
 {
-	
+	next = next * 1104515245 + 12345;
+    return (unsigned int) (next / 65536) % 32768;
 }
 
 
 void srand(unsigned int seed)
 {
-	
+	next = seed;
 }
