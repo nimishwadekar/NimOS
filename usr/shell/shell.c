6c06ef038338ceea4a432a3fcb1ef6fc3f3aef7b
Nimish Wadekar <wadekar.nimish@gmail.com>
2021-10-06 18:34:24 +0530

    Shell stub

--- usr/shell/shell.c
+++ usr/shell/shell.c
@@ -3,23 +3,12 @@
 #include <string.h>
 #include <graphics.h>
 #include <unistd.h>
+#include <math.h>
 
 int main()
 {
-    crsr_pos_t c = getcrsrpos();
-    setcrsrpos(750, 20);
-    printf("Date");
-    setcrsrpos(c.x, c.y);
-    if(lockscr(700, 0, 800, 60) == -1)
-    {
-        printf("Couldn't lock screen\n");
-        while(1);
-    }
-
-    drawline(50, 50, 750, 50, 0xFFFFFF);
-    drawline(50, 150, 750, 150, 0xFFFFFF);
-
-    while(1) getchar();
+    double d;
+    printf("Shell\n");
     
     while(1);
 }
