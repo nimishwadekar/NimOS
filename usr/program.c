4834d04de48f52dd7cf2b9983b946e77216c6f32
Nimish Wadekar <wadekar.nimish@gmail.com>
2021-10-08 02:17:49 +0530

    Updated program

--- usr/program.c
+++ usr/program.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
-    int a;
-    printf("Enter: ");
-    scanf("%d", &a);
-    printf("%d was entered\n", a);
+    int a, b;
+    printf("Enter 2 numbers: ");
+    scanf("%d %d", &a, &b);
+    printf("%d + %d = %d\n\n", a, b, a + b);
+
+    FILE *f = fopen("anotherDirectory/src/hello.c", "r");
+    fseek(f, 0, SEEK_END);
+    long size = ftell(f);
+    fseek(f, 0, SEEK_SET);
+
+    char *buf = malloc(size);
+    fread(buf, 1, size, f);
+    fclose(f);
+
+    printf("%s\n", buf);
 
     return 0;
 }
