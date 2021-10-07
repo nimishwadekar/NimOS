8dbfea845b45981f83e3edc912386549a5ff8d00
Nimish Wadekar <wadekar.nimish@gmail.com>
2021-10-07 11:23:04 +0530

    Add invalid command handler

--- usr/shell/command.c
+++ usr/shell/command.c
@@ -7,16 +7,16 @@ static enum COMMANDS identifyCommand(char *cmdname)
 {
     switch(cmdname[0])
     {
-        case 'e':
-        if(strcmp(cmdname + 1, "xit") == 0) return CMD_EXIT;
-        break;
-
         case 'h':
         if(strcmp(cmdname + 1, "elp") == 0) return CMD_HELP;
         break;
 
         case 'r':
         if(strcmp(cmdname + 1, "un") == 0) return CMD_RUN;
+        break;
+
+        case 's':
+        if(strcmp(cmdname + 1, "hutdown") == 0) return CMD_SHUTDOWN;
         break;
 
         case 't':
@@ -31,4 +31,23 @@ void runCommand(char *cmd)
 {
     char *tok = strtok(cmd, " ");
     enum COMMANDS cmdtype = identifyCommand(tok);
+
+    switch(cmdtype)
+    {
+        case CMD_HELP:
+        break;
+
+        case CMD_RUN:
+        break;
+
+        case CMD_SHUTDOWN:
+        break;
+
+        case CMD_TIME:
+        break;
+
+        case CMD_INVALID:
+        printf("%s: Unrecognized command\n", tok);
+        break;
+    }
 }
--- usr/shell/command.h
+++ usr/shell/command.h
@@ -4,10 +4,10 @@
 enum COMMANDS
 {
     CMD_INVALID = -1,
-    CMD_RUN,
     CMD_HELP,
-    CMD_TIME,
-    CMD_EXIT
+    CMD_RUN,
+    CMD_SHUTDOWN,
+    CMD_TIME
 };
 
 void runCommand(char *cmd);
