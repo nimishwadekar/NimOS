8dbc8a6c9e402bb367438459116860090ab6f450
Nimish Wadekar <80649397+nimishwadekar@users.noreply.github.com>
2021-10-14 00:59:15 +0530

    Update run.cmd

--- run.cmd
+++ run.cmd
@@ -1,1 +1,1 @@
-qemu-system-x86_64 -machine q35 -cpu qemu64 -bios bin/OVMF.fd -m 64 -drive file=bin/TokyoOS.img,format=raw -rtc clock=host -serial file:log/serial.log
+qemu-system-x86_64 -machine q35 -cpu qemu64 -bios bin/OVMF.fd -m 64 -drive file=bin/TokyoOS.img,format=raw -rtc clock=host -serial file:serial.log
